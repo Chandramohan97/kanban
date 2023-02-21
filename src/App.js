25bd35386d30863bf015a81c909f21c465d62947
Chandramohan97 <chandramsriv@gmail.com>
2023-02-21 10:57:42 +0530

    style : global css file for commonly used element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Board from "./Components/Board/Board.js";
+import Sidebar from "./Components/SideBar/Sidebar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="app_navbar">
+        <h2>Project Name</h2>
+      </div>
+      <div className="app_outer">
+        <div className="app_boards">
+          <Board />
+          <Board />
+          <Board />
+          <Board />
+          <Board />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Board.css";
+import "../Global/global.css";
 
 const Board = () => {
   return (
-    <div className="Board">
+    <div className="board">
+      <div className="board_top">
+        <p className="board_top_title">To Do</p>
+      </div>
       <h1>Board</h1>
     </div>
   );
