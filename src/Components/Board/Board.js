b451ae46cbcf05e66e973f6ffd8c9e6784ff309b
Chandramohan97 <chandramsriv@gmail.com>
2023-02-21 10:39:38 +0530

    feat : Board Component(styling and Js file)

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Board.css";
+
+const Board = () => {
+  return (
+    <div className="Board">
+      <h1>Board</h1>
+    </div>
+  );
+};
+
+export default Board;
