3333f21a3feee3540c9da1ec00038f3f3f23909a
Chandramohan97 <chandramsriv@gmail.com>
2023-02-21 12:55:02 +0530

    Board Component update : styling and features.

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Board.css";
-import "../Global/global.css";
+import "../../Global/global.css";
+import { MoreHorizontal } from "react-feather";
 
 const Board = () => {
   return (
     <div className="board">
       <div className="board_top">
-        <p className="board_top_title">To Do</p>
+        <p>
+          To Do <span>2</span> <MoreHorizontal />
+        </p>
       </div>
-      <h1>Board</h1>
+      <div className="board_cards">
+        <h3>Cards 1</h3>
+        <h3>Cards 2</h3>
+        <h3>Cards 2</h3>
+      </div>
     </div>
   );
 };
