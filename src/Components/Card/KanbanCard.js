f5f08a21ff0226cea09174da392d391c355a02f8
Chandramohan97 <chandramsriv@gmail.com>
2023-03-01 13:25:00 +0530

    feat : Kanban card

---
+++ src/Components/Card/KanbanCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card, CardHeader } from '@chakra-ui/react'
+
+
+const KanbanCard = () => {
+  return (
+    <Card border={"1px solid"} width="60%">
+        <CardHeader>
+            <p>Hello</p>
+        </CardHeader>
+    </Card>
+  )
+}
+
+export default KanbanCard
