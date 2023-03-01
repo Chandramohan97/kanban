11da945b17b207323e6b209f2a04942691640fb4
Chandramohan97 <chandramsriv@gmail.com>
2023-03-01 16:52:07 +0530

    Kanban card

--- src/Components/Card/KanbanCard.js
+++ src/Components/Card/KanbanCard.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Button, Card, CardHeader,CardBody} from '@chakra-ui/react'
+import { Button, Card, CardHeader,CardBody,Text} from '@chakra-ui/react'
 
 
 const KanbanCard = (props) => {
   return (
-    <Card border={"1px solid"} width="60%" mx="5%" flexDirection={"column"} alignItems="flex-start">
-        <CardHeader>
-            <Button height="20px" background={"green.200"} my="-20px">{props.priority}</Button>
-            <CardBody>h</CardBody>
+    <Card border={"1px solid"} width="80%" mx="5%" flexDirection={"column"} alignItems="flex-start">
+        <CardHeader flexDirection={"column"} alignItems="left">
+            <Button position="relative" fontSize="13px" left="-10px" top="-20px" width="20px" borderRadius="40px" height="20px" background="green.200" my="-20px">{props.priority}</Button>
+            <CardBody position={"relative"} left="-25px" top="-20px">
+              <Text fontSize={"m"} width={"90%"}/>This is a nex text
+            </CardBody>
         </CardHeader>
     </Card>
   )
